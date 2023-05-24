ed401ddf4a0138401e924e008c848e164a07d284
joker <ser.fyodoroff@yandex.ru>
2023-05-25 02:11:00 +0300

    An old project with spring rest basics

---
+++ src/main/java/ru/joker/spring/SpringRestApp/services/PeopleService.java
@@ -0,0 +1,33 @@
+package ru.joker.spring.SpringRestApp.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.joker.spring.SpringRestApp.models.Person;
+import ru.joker.spring.SpringRestApp.repositories.PeopleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class PeopleService {
+
+    private PeopleRepository peopleRepository;
+
+    @Autowired
+    public PeopleService(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+    public List<Person> findAll() {
+        return peopleRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> person = peopleRepository.findById(id);
+        return person.orElse(null);
+    }
+
+}
---
+++ src/test/java/ru/joker/spring/SpringRestApp/SpringRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.joker.spring.SpringRestApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
